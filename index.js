998748a24d3b4e72c720fbd808b194c4aa4147d3
Zoriana <zoriana.shatkovska@gmail.com>
2022-11-02 19:52:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+
+function findMatching(drivers, string){
+    return drivers.filter((el) => 
+    el.toLowerCase().includes(string.toLowerCase()));
+}
+
+function fuzzyMatch (drivers, string){
+    return drivers.filter((el) => 
+    el.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+
+function matchName(driver, string){
+    return driver.filter((el) => 
+    el.name === string)
+}
